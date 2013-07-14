e677d5b89b367f4be6676f4977ba30e8a23e4e37
Michael Vorburger <mike@vorburger.ch>
2013-07-14 21:17:43 +0200

    switched from stock routeProvider to modern angular ui router

--- angular-addressbook/app/scripts/app.js
+++ angular-addressbook/app/scripts/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
-angular.module('mui.jsAngularAddressbookApp', ['ui.date', 'ngGrid'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
-      .otherwise({ redirectTo: '/' });
+angular.module('mui.jsAngularAddressbookApp', ['ui.state', 'ui.date', 'ngGrid'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, send to / (TODO what does it do without this?!)
+    $urlRouterProvider.otherwise('/a');
+
+    $stateProvider
+    .state('route1', { url: '/a', templateUrl: 'views/main.html', controller: 'MainCtrl' });
   });
