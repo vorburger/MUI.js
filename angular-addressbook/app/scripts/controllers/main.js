2a5480a48e70ff95311be21f4cade2c3fc3fc091
Michael Vorburger <mike@vorburger.ch>
2013-07-14 16:34:44 +0200

    AngularJS Data Binding rocks

--- angular-addressbook/app/scripts/controllers/main.js
+++ angular-addressbook/app/scripts/controllers/main.js
@@ -2,24 +2,22 @@
 
 angular.module('mui.jsAngularAddressbookApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
     
     $scope.mySelections = [];
     
-    $scope.myData = [{name: 'Moroni', age: 50},
-                     {name: 'Teancum', age: 43},
-                     {name: 'Jacob', age: 27},
-                     {name: 'Nephi', age: 29},
-                     {name: 'Enos', age: 34}];
+    // TODO Date String formats?!?
+    $scope.myData = [{id: 1, name: 'Moroni', age: 50, email: 'moron@blue.com', country: 'Switzerland', phone: '78 837 31 33', since: '05/01/2013'},
+                     {id: 2, name: 'Teancum', age: 43},
+                     {id: 3, name: 'Jacob', age: 27},
+                     {id: 4, name: 'Nephi', age: 29},
+                     {id: 5, name: 'Enos', age: 34}];
+                     
     $scope.myGridOptions = { data: 'myData',
             selectedItems: $scope.mySelections,
-            columnDefs: [{field:'name', displayName:'Name'}, {field:'age', displayName:'Age'}],
+            columnDefs: [{field: 'name', displayName: 'Name'}, {field: 'age', displayName: 'Age'}],
             enableRowSelection: true, multiSelect: false };
-    // you can also specify data as: $scope.myOptions = { data: $scope.myData }. 
+            
+    // you can also specify data as: $scope.myGridOptions = { data: $scope.myData }.
     // However, updates to the underlying data will not be reflected in the grid
     
   });
