f5bd6b673c4fc256a4e2d1ddc0646d3ca375ade9
Michael Vorburger <mike@vorburger.ch>
2013-07-14 23:03:39 +0200

    new Home state/view

--- angular-addressbook/app/scripts/app.js
+++ angular-addressbook/app/scripts/app.js
@@ -5,14 +5,12 @@ angular.module('mui.jsAngularAddressbookApp', ['ui.state', 'ui.date', 'ngGrid'])
     // TODO how to read this configuration from a app.js.routes.json? (which will be slower.. how to inline it again, later?)
   
     // For any unmatched url, or when there is no when there is no route, send to default state URL
-    $urlRouterProvider.otherwise('/main/contacts');
-    $urlRouterProvider.when('', '/main/contacts');
+    $urlRouterProvider.otherwise('/main/home');
+    $urlRouterProvider.when('', '/main/home');
 
     $stateProvider // nota bene: better to NOT (ever) use just url: '/' !
-    .state('main', { url: '/main', views: { 'root': { templateUrl: 'views/main.html' /*, controller: 'MainCtrl' */}}})
-	.state('main.contacts', { url: '/contacts', views: {
-		  // 'root': { templateUrl: 'views/main.html' /*, controller: 'MainCtrl' */},
-		  'main-body': { templateUrl: 'views/contacts.html', controller: 'MainCtrl' }}
-		  });
+    .state('main', { url: '/main', abstract: true, views: { 'root': { templateUrl: 'views/main.html' }}})
+	.state('main.home', { url: '/home', views: { 'main-body': { templateUrl: 'views/home.html' }}})
+	.state('main.contacts', { url: '/contacts', views: { 'main-body': { templateUrl: 'views/contacts.html', controller: 'MainCtrl' }}});
 	// note, when gen. later: Alternately (i.e. instead of dot), you can specify the parent of a state via the 'parent' property.
   });
