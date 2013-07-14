497e77196d1fd80cf79d2cfd3fb9e90544300746
Michael Vorburger <mike@vorburger.ch>
2013-07-14 22:30:27 +0200

    working named nested views (layout broken, fixing next)

--- angular-addressbook/app/scripts/app.js
+++ angular-addressbook/app/scripts/app.js
@@ -4,9 +4,15 @@ angular.module('mui.jsAngularAddressbookApp', ['ui.state', 'ui.date', 'ngGrid'])
   .config(function ($stateProvider, $urlRouterProvider) {
     // TODO how to read this configuration from a app.js.routes.json? (which will be slower.. how to inline it again, later?)
   
-    // For any unmatched url, send to / (TODO what does it do without this?!)
-    $urlRouterProvider.otherwise('/a');
+    // For any unmatched url, or when there is no when there is no route, send to default state URL
+    $urlRouterProvider.otherwise('/main/contacts');
+    $urlRouterProvider.when('', '/main/contacts');
 
-    $stateProvider
-    .state('route1', { url: '/a', views: { 'root': { templateUrl: 'views/main.html', controller: 'MainCtrl' }}});
+    $stateProvider // nota bene: better to NOT (ever) use just url: '/' !
+    .state('main', { url: '/main', views: { 'root': { templateUrl: 'views/main.html' /*, controller: 'MainCtrl' */}}})
+	.state('main.contacts', { url: '/contacts', views: {
+		  // 'root': { templateUrl: 'views/main.html' /*, controller: 'MainCtrl' */},
+		  'main-body': { templateUrl: 'views/contacts.html', controller: 'MainCtrl' }}
+		  });
+	// note, when gen. later: Alternately (i.e. instead of dot), you can specify the parent of a state via the 'parent' property.
   });
