115ab5640a675b783f11f8996268bde284eaaf79
Michael Vorburger <mike@vorburger.ch>
2013-07-14 21:21:48 +0200

    named view

--- angular-addressbook/app/scripts/app.js
+++ angular-addressbook/app/scripts/app.js
@@ -2,9 +2,11 @@
 
 angular.module('mui.jsAngularAddressbookApp', ['ui.state', 'ui.date', 'ngGrid'])
   .config(function ($stateProvider, $urlRouterProvider) {
+    // TODO how to read this configuration from a app.js.routes.json? (which will be slower.. how to inline it again, later?)
+  
     // For any unmatched url, send to / (TODO what does it do without this?!)
     $urlRouterProvider.otherwise('/a');
 
     $stateProvider
-    .state('route1', { url: '/a', templateUrl: 'views/main.html', controller: 'MainCtrl' });
+    .state('route1', { url: '/a', views: { 'root': { templateUrl: 'views/main.html', controller: 'MainCtrl' }}});
   });
