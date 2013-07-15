eed5817c5b6fc0f25e8b0b03c20de9f0dca8696e
Michael Vorburger <mike@vorburger.ch>
2013-07-15 02:05:56 +0200

    BROKEN Menu with a ui-sref & ng-class="{ active: .includes

--- angular-addressbook/app/scripts/app.js
+++ angular-addressbook/app/scripts/app.js
@@ -7,10 +7,18 @@ angular.module('mui.jsAngularAddressbookApp', ['ui.state', 'ui.date', 'ngGrid'])
     // For any unmatched url, or when there is no when there is no route, send to default state URL
     $urlRouterProvider.otherwise('/main/home');
     $urlRouterProvider.when('', '/main/home');
+    // when is also useful for "aliases":
+    $urlRouterProvider.when('/main/kontakte', '/main/contacts');
 
     $stateProvider // nota bene: better to NOT (ever) use just url: '/' !
     .state('main', { url: '/main', abstract: true, views: { 'root': { templateUrl: 'views/main.html' }}})
 	.state('main.home', { url: '/home', views: { 'main-body': { templateUrl: 'views/home.html' }}})
 	.state('main.contacts', { url: '/contacts', views: { 'main-body': { templateUrl: 'views/contacts.html', controller: 'MainCtrl' }}});
 	// note, when gen. later: Alternately (i.e. instead of dot), you can specify the parent of a state via the 'parent' property.
-  });
+  })
+  // https://github.com/angular-ui/ui-router/wiki/Quick-Reference#note-about-using-state-within-a-template
+  .run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+  })
+  ;
