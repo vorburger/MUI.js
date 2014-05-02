25e3b3c22911c8ccd97dc71b88ee76389d2e547b
Michael Vorburger <mike@vorburger.ch>
2014-05-02 23:33:27 +0200

    moved ui router definitions to sep. (to-be-gen..) file
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- angular-addressbook/app/scripts/app.js
+++ angular-addressbook/app/scripts/app.js
@@ -72,24 +72,6 @@ angular.module('mui.jsAngularAddressbookApp', ['ui.state', 'ui.date', 'ngGrid', 
     return newContactsStoreService;
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
-	  
-    // For any unmatched url, or when there is no when there is no route, send to default state URL
-    $urlRouterProvider.otherwise('/main/home');
-    $urlRouterProvider.when('', '/main/home');
-    // when is also useful for "aliases":
-    $urlRouterProvider.when('/main/kontakte', '/main/contacts');
-
-    $stateProvider // nota bene: better to NOT (ever) use just url: '/' !
-    .state('main', { url: '/main', abstract: true, views: { 'root': { templateUrl: 'views/main.html' }}})
-	.state('main.home', { url: '/home', title: 'Welcome!', views: { 'mainBody': { templateUrl: 'views/home.html' }}})
-	.state('main.contacts', { url: '/contacts', title: 'Contacts', views: { 'mainBody': { templateUrl: 'views/contacts.html', controller: 'ContactsCtrlClassic' }}})
-    .state('main.contactsMUI', { url: '/contactsMUI', title: 'Contacts', views: { 'mainBody': { templateUrl: 'views/meta/datagrid.html', controller: 'ContactsCtrlMUI' }}})
-	.state('main.contact', { url: '/contact/HTMLTemplate/{id}', title: 'Edit/Add Contact', views: { 'mainBody': { templateUrl: 'views/contact.html', controller: 'AContactCtrl' }}})
-    .state('main.contactMUI', { url: '/contact/GenForm/{id}', title: 'Edit/Add Contact', views: { 'mainBody': { templateUrl: 'views/meta/simpleform.html', controller: 'AContactCtrl' }}});
-	// note, when gen. later: Alternately (i.e. instead of dot), you can specify the parent of a state via the 'parent' property.
-  })
-
   // https://github.com/angular-ui/ui-router/wiki/Quick-Reference#note-about-using-state-within-a-template
   // .run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
   .run(function ($rootScope, $state, $stateParams) {
---
+++ angular-addressbook/app/scripts/routes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*global angular:true*/
+angular.module('mui.jsAngularAddressbookApp') // NOTE: Do *NOT* ,[...]) here!! http://stackoverflow.com/questions/16771812/angularjs-seed-putting-javascript-into-separate-files-app-js-controllers-js
+  .config(function ($stateProvider, $urlRouterProvider) {
+	  
+    // For any unmatched url, or when there is no when there is no route, send to default state URL
+    $urlRouterProvider.otherwise('/main/home');
+    $urlRouterProvider.when('', '/main/home');
+    // when is also useful for "aliases":
+    $urlRouterProvider.when('/main/kontakte', '/main/contacts');
+
+    $stateProvider // nota bene: better to NOT (ever) use just url: '/' !
+    .state('main', { url: '/main', abstract: true, views: { 'root': { templateUrl: 'views/main.html' }}})
+	.state('main.home', { url: '/home', title: 'Welcome!', views: { 'mainBody': { templateUrl: 'views/home.html' }}})
+
+	.state('main.contacts', { url: '/contacts', title: 'Contacts', views: { 'mainBody': { templateUrl: 'views/contacts.html', controller: 'ContactsCtrlClassic' }}})
+
+    .state('main.contactsMUI', { url: '/contactsMUI', title: 'Contacts', views: { 'mainBody': { templateUrl: 'views/meta/datagrid.html', controller: 'ContactsCtrlMUI' }}})
+	.state('main.contact', { url: '/contact/HTMLTemplate/{id}', title: 'Edit/Add Contact', views: { 'mainBody': { templateUrl: 'views/contact.html', controller: 'AContactCtrl' }}})
+
+
+    .state('main.contactMUI', { url: '/contact/GenForm/{id}', title: 'Edit/Add Contact', views: { 'mainBody': { templateUrl: 'views/meta/simpleform.html', controller: 'AContactCtrl' }}});
+
+	// note, when gen. later: Alternately (i.e. instead of dot), you can specify the parent of a state via the 'parent' property.
+  });
